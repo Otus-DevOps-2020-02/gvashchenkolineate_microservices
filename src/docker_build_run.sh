0433086f690ab19aa70619d109128659aee35887
Georgy Vashchenko <gvashchenko@lineate.com>
2020-05-15 01:56:51 +0600

    Optimize Dockerfile: Build from Ubuntu and Alpine Add notes and image size diff

--- src/docker_build_run.sh
+++ src/docker_build_run.sh
@@ -18,3 +18,23 @@ docker run -d --network=reddit --network-alias=post_db --network-alias=comment_d
 docker run -d --network=reddit --network-alias=post $DOCKERHUB_LOGIN/post:1.0
 docker run -d --network=reddit --network-alias=comment $DOCKERHUB_LOGIN/comment:1.0
 docker run -d --network=reddit -p 9292:9292 $DOCKERHUB_LOGIN/ui:1.0
+
+# ---------------------------------------------------------------
+
+echo Build optimized ui image from ubuntu
+docker build -t $DOCKERHUB_LOGIN/ui:2.0 ./ui
+
+echo Build optimized ui image from alpine
+docker build -t $DOCKERHUB_LOGIN/ui:3.0 -f ./ui/Dockerfile.3 ./ui
+
+echo Build optimized comment image from alpine
+docker build -t $DOCKERHUB_LOGIN/comment:2.0 ./comment
+
+# Image diff
+docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.Size}}" | grep gvashchenko | sort
+#  gvashchenko/comment     1.0    752MB
+#  gvashchenko/comment     2.0    243MB
+#  gvashchenko/post        1.0    110MB
+#  gvashchenko/ui          1.0    786MB
+#  gvashchenko/ui          2.0    462MB
+#  gvashchenko/ui          3.0    246MB
