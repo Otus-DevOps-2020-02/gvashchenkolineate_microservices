a64f2dc003814c3a067387435bd747463b392fa9
Georgy Vashchenko <gvashchenko@lineate.com>
2020-05-18 00:02:58 +0600

    Docker: Fix build (add ruby-full)

--- src/docker_build_run.sh
+++ src/docker_build_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+echo Kill running containers
+docker kill $(docker ps -q)
+
 echo Set dockerhub login for next command substitution
 DOCKERHUB_LOGIN=gvashchenko
 
@@ -19,7 +22,7 @@ docker run -d --network=reddit --network-alias=post $DOCKERHUB_LOGIN/post:1.0
 docker run -d --network=reddit --network-alias=comment $DOCKERHUB_LOGIN/comment:1.0
 docker run -d --network=reddit -p 9292:9292 $DOCKERHUB_LOGIN/ui:1.0
 
-# ---------------------------------------------------------------
+# --------------------------------------------------------------- Image minization
 
 echo Build optimized ui image from ubuntu
 docker build -t $DOCKERHUB_LOGIN/ui:2.0 ./ui
@@ -38,3 +41,11 @@ docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.Size}}" | grep gvash
 #  gvashchenko/ui          1.0    786MB
 #  gvashchenko/ui          2.0    462MB
 #  gvashchenko/ui          3.0    246MB
+
+# --------------------------------------------------------------- Run minimized images
+
+echo Run docker containers from minimized images
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=reddit --network-alias=post $DOCKERHUB_LOGIN/post:1.0
+docker run -d --network=reddit --network-alias=comment $DOCKERHUB_LOGIN/comment:2.0
+docker run -d --network=reddit -p 9292:9292 $DOCKERHUB_LOGIN/ui:3.0
