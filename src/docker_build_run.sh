4664ad85fd05c04cc7bd99d55173d8dc57a030f2
Georgy Vashchenko <gvashchenko@lineate.com>
2020-05-19 12:59:06 +0600

    Add front_net & back_net, networks investigation commands Add ifconfig outputs

--- src/docker_build_run.sh
+++ src/docker_build_run.sh
@@ -67,3 +67,33 @@ docker run -d --network=reddit --network-alias=post_db --network-alias=comment_d
 docker run -d --network=reddit --network-alias=post $DOCKERHUB_LOGIN/post:1.0
 docker run -d --network=reddit --network-alias=comment $DOCKERHUB_LOGIN/comment:2.0
 docker run -d --network=reddit -p 9292:9292 $DOCKERHUB_LOGIN/ui:3.0
+
+# ---------------------------------------------------------------- Network experiments
+
+echo Create a separate bridge-networks
+docker network create back_net --subnet=10.0.2.0/24
+docker network create front_net --subnet=10.0.1.0/24
+
+echo Run services in two bridge-networks so that ui has no access to database
+docker run -d --network=back_net --name mongo_db --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=front_net -p 9292:9292 --name ui  $DOCKERHUB_LOGIN/ui:1.0
+docker run -d --network=back_net --name comment  $DOCKERHUB_LOGIN/comment:1.0
+docker run -d --network=back_net --name post  $DOCKERHUB_LOGIN/post:1.0
+
+echo Connect post & comment containers to front_net
+docker network connect front_net post
+docker network connect front_net comment
+
+# ----------------------------------------------------------------- Commands for network investigations
+
+# Watch current net namespaces
+sudo ln -s /var/run/docker/netns /var/run/netns
+sudo ip netns
+
+# Install bridge-utils
+sudo apt-get update && sudo apt-get install bridge-utils
+docker network ls
+ifconfig | grep br
+brctl show <br-interface>
+sudo iptables -nL -t nat -v
+ps ax | grep docker-proxy
