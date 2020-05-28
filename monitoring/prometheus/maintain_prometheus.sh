365db4ff09a663bd77790996aab391b57fad29ac
Georgy Vashchenko <gvashchenko@lineate.com>
2020-05-28 17:56:06 +0600

    Prometheus: add to docker-compose along with reddit services Docker-compose: use pre-built images

--- monitoring/prometheus/maintain_prometheus.sh
+++ monitoring/prometheus/maintain_prometheus.sh
@@ -13,3 +13,10 @@ docker stop prometheus
 export USER_NAME=gvashchenko
 
 docker build -t $USER_NAME/prometheus .
+
+# Running `docker/docker-compose up -d` uses `docker/docker-compose.override.yml`
+# and when using docker-machine with GCP it may cause a problem of failed reddit-services
+# because volumes mounted to containers are not the files on your localhost
+# but are supposed to be the files on docker-machine itself.
+# Solution: use this command
+docker-compose -f docker-compose.yml up -d
