d487aace216397a87a735c3f872aaffb8163976b
Georgy Vashchenko <gvashchenko@lineate.com>
2020-06-09 00:58:41 +0600

    Prometheus: Collect Stackdriver metrics

--- monitoring/create_docker_machine.sh
+++ monitoring/create_docker_machine.sh
@@ -8,4 +8,6 @@ docker-machine create --driver google \
     --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
     --google-machine-type n1-standard-1 \
     --google-zone europe-west1-b \
+    # TODO should create an instance without this explicitly set scope to copy default scopes here because it overrides all others
+    --google-scopes "https://www.googleapis.com/auth/monitoring.read"\
     docker-host
