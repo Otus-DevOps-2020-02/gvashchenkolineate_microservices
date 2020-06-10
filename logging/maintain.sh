12966095f6c02df2ec455e706af63a5c90ca97ac
Georgy Vashchenko <gvashchenko@lineate.com>
2020-06-10 22:31:11 +0600

    Logging: Tracing: Add Zipkin to logging services

---
+++ logging/maintain.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# configure local env
+eval $(docker-machine env docker-host)
+
+export USER_NAME=gvashchenko
+
+# Build all reddit app components images
+for i in ui post-py comment; do cd src/$i; bash docker_build.sh; docker push $USER_NAME/$i; cd -; done
+
+cd docker
+
+# Run or create and run all app services and logging services
+docker-compose -f docker-compose.yml -f docker-compose-logging.yml up -d
+
+# Check containers are running
+docker-compose -f docker-compose.yml -f docker-compose-logging.yml ps
