0965c71c88fff782c7cf318f3d0ad4b1450f975e
Georgy Vashchenko <gvashchenko@lineate.com>
2020-07-21 14:28:39 +0600

    Kubernetes: Terraform: Extract logging, monitoring, oauth_scopes as variables

--- kubernetes/terraform/main.tf
+++ kubernetes/terraform/main.tf
@@ -37,18 +37,14 @@ resource "google_container_cluster" "kubernetes-cluster" {
 
   enable_legacy_abac = var.legacy_authorization
 
+  logging_service = var.logging_service
+  monitoring_service = var.monitoring_service
+
   node_config {
     machine_type = var.machine_type
     disk_size_gb = var.node_disk_size_gb
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
+    oauth_scopes = var.oauth_scopes
 
     metadata = {
       disable-legacy-endpoints = "true"
